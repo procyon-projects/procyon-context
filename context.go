e3dda58cc8768e2d14e336a7fc877a900084b6c0
Burak Koken <burakkokenn@gmail.com>
2020-05-18 21:31:16 +0200

    Added context interfaces for application. they will be implemented soon.

---
+++ context.go
@@ -0,0 +1,38 @@
+package context
+
+import (
+	"github.com/Rollcomp/procyon-core"
+	"github.com/Rollcomp/procyon-web/server"
+)
+
+type ApplicationContext interface {
+	GetApplicationName() string
+	GetStartupTimeStamp() int64
+}
+
+type WebApplicationContext interface {
+	ApplicationContext
+}
+
+type ConfigurableApplicationContext interface {
+	SetEnvironment(environment core.ConfigurableEnvironment)
+	GetEnvironment() core.ConfigurableEnvironment
+}
+
+type WebServerApplicationContext struct {
+	webServer *server.WebServer
+}
+
+func NewWebServerApplicationContext() *WebServerApplicationContext {
+	return &WebServerApplicationContext{}
+}
+
+func (ctx *WebServerApplicationContext) createWebServer() {
+	if ctx.webServer == nil {
+		factory := server.NewDefaultWebServerFactory()
+		ctx.webServer = factory.GetWebServer()
+	}
+}
+
+type DefaultWebServerApplicationContext struct {
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Rollcomp/procyon-context
 
 go 1.13
+
+require (
+	github.com/Rollcomp/procyon-core v0.0.0-20200518190211-05f19133892b
+	github.com/Rollcomp/procyon-web v0.0.0-20200518192228-ea76f20fadab
+)
