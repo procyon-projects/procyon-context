fa4735d07c2abdd69810083904c63cbca9b5048f
Burak Koken <burakkokenn@gmail.com>
2020-05-31 13:39:09 +0200

    Added constructor functions to create Repository and Service metadata instances

--- components.go
+++ components.go
@@ -1,13 +1,26 @@
 package context
 
+import core "github.com/Rollcomp/procyon-core"
+
 type RepositoryMetadata struct {
+	typ *core.Type
 }
 
 type Repository interface {
 	GetRepositoryMetadata() RepositoryMetadata
 }
 
+func NewRepositoryMetadata(typ *core.Type) RepositoryMetadata {
+	return RepositoryMetadata{
+		typ,
+	}
+}
+
 type ServiceMetadata struct {
+}
+
+func NewServiceMetadata() ServiceMetadata {
+	return ServiceMetadata{}
 }
 
 type Service interface {
