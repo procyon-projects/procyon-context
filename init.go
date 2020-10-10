3fd90e030a1940e7b3d9d5e16d520028134939f8
Burak Köken <burakkokenn@gmail.com>
2020-10-10 21:42:02 +0200

    Removed the call of method which was forgotten.

--- init.go
+++ init.go
@@ -3,8 +3,6 @@ package context
 import core "github.com/procyon-projects/procyon-core"
 
 func init() {
-	/* Initialize Pools */
-	initBaseApplicationContextPool()
 	/* Configuration Properties Binding Processor */
 	core.Register(NewConfigurationPropertiesBindingProcessor)
 }
