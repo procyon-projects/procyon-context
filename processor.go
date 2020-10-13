7b4d38971c97d6fcea5a58c2f14f41f143c2b277
Burak Köken <burakkokenn@gmail.com>
2020-10-13 12:47:30 +0200

    Completed the implementation of the bootstrap processors to scan and register pea definitions for now

--- processor.go
+++ processor.go
@@ -6,11 +6,16 @@ type BootstrapProcessor struct {
 }
 
 func (processor BootstrapProcessor) AfterPeaDefinitionRegistryInitialization(registry peas.PeaDefinitionRegistry) {
-	// do something
+	processor.processPeaDefinitions(registry)
 }
 
 func (processor BootstrapProcessor) AfterPeaFactoryInitialization(factory peas.ConfigurablePeaFactory) {
 	// do something
+}
+
+func (processor BootstrapProcessor) processPeaDefinitions(registry peas.PeaDefinitionRegistry) {
+	scanner := NewComponentPeaDefinitionScanner(registry)
+	scanner.DoScan()
 }
 
 type EventListenerProcessor struct {
