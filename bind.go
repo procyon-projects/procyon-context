eef0af7de215bd54a3420411b57966abdcddc926
Burak Köken <burakkokenn@gmail.com>
2020-10-20 22:16:49 +0200

    Updated the implementation of ConfigurationPropertiesBindingProcessor and its signature

--- bind.go
+++ bind.go
@@ -1,39 +1,40 @@
 package context
 
-import "errors"
+import (
+	"errors"
+	core "github.com/procyon-projects/procyon-core"
+)
 
 type ConfigurationPropertiesBindingProcessor struct {
-	binder *ConfigurationPropertiesBinder
+	binder ConfigurationPropertiesBinder
 }
 
-func NewConfigurationPropertiesBindingProcessor(binder *ConfigurationPropertiesBinder) *ConfigurationPropertiesBindingProcessor {
-	return &ConfigurationPropertiesBindingProcessor{
-		binder,
+func NewConfigurationPropertiesBindingProcessor(env core.Environment, typeConverterService core.TypeConverterService) ConfigurationPropertiesBindingProcessor {
+	return ConfigurationPropertiesBindingProcessor{
+		newConfigurationPropertiesBinder(env, typeConverterService),
 	}
 }
 
-func (processor *ConfigurationPropertiesBindingProcessor) SetApplicationContext(context ApplicationContext) {
+func (processor ConfigurationPropertiesBindingProcessor) SetApplicationContext(context ApplicationContext) {
 
 }
 
-func (processor *ConfigurationPropertiesBindingProcessor) AfterProperties() {
+func (processor ConfigurationPropertiesBindingProcessor) AfterProperties() {
 
 }
 
-func (processor *ConfigurationPropertiesBindingProcessor) BeforePeaInitialization(peaName string, pea interface{}) (interface{}, error) {
-	if processor.binder != nil {
-		err := processor.binder.Bind(pea)
-		if err != nil {
-			return nil, errors.New("error occurred while configuration properties was being binding to pea instance : " + peaName)
-		}
+func (processor ConfigurationPropertiesBindingProcessor) BeforePeaInitialization(peaName string, pea interface{}) (interface{}, error) {
+	err := processor.binder.Bind(pea)
+	if err != nil {
+		return nil, errors.New("error occurred while configuration properties was being binding to pea instance : " + peaName)
 	}
 	return pea, nil
 }
 
-func (processor *ConfigurationPropertiesBindingProcessor) Initialize() error {
+func (processor ConfigurationPropertiesBindingProcessor) Initialize() error {
 	return nil
 }
 
-func (processor *ConfigurationPropertiesBindingProcessor) AfterPeaInitialization(peaName string, pea interface{}) (interface{}, error) {
+func (processor ConfigurationPropertiesBindingProcessor) AfterPeaInitialization(peaName string, pea interface{}) (interface{}, error) {
 	return pea, nil
 }
